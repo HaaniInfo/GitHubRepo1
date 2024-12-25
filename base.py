5576af29287d1af93a0417d6aefdefe3d384accc
Ansaf Haani <Ansafhaani@gmail.com>
2024-12-25 12:44:31 +0530

    Modified

--- base.py
+++ base.py
@@ -1,3 +1,7 @@
 name ="sanjeevan"
 name1 ="ahamed"
 print ( "My name is " + name + " " + name1 )
+
+
+length = input("Enter the length of the table : ")
+height = input("Enter the height of the table : ")
