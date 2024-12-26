10e7931337dbf307b83f84a03e3b6d0b8c62e01a
Ansaf Haani <Ansafhaani@gmail.com>
2024-12-26 13:35:44 +0530

    Base.py Modified

--- base.py
+++ base.py
@@ -3,5 +3,7 @@ name1 ="ahamed"
 print ( "My name is " + name + " " + name1 )
 
 
-length = input("Enter the length of the table : ")
-height = input("Enter the height of the table : ")
+length = int(input("Enter the length of the table : "))
+height = int(input("Enter the height of the table : "))
+# Circumcision = str(2*(length + height))
+print ("Circumcision is " + str(2*(length + height)))
