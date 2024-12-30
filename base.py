f804a0180f3a341f1f4755ba8bfb751b439d32d8
HaaniInfo <Ansafhaani145@Gmail.com>
2024-12-30 13:44:11 +0530

    Update base.py

--- base.py
+++ base.py
@@ -3,9 +3,6 @@ name1 ="ahamed"
 print ( "My name is " + name + " " + name1 )
 
 
-length = int(input("Enter the length of the table : "))
-height = int(input("Enter the height of the table : "))
-# Circumcision = str(2*(length + height))
-print ("Circumcision is " + str(2*(length + height)))
+
 
 
