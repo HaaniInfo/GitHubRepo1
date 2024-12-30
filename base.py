96f48cb69f9091143dca12d9fd0644c96e37c12a
Ansaf Haani <Ansafhaani@gmail.com>
2024-12-30 13:41:58 +0530

    Python fie updated

--- base.py
+++ base.py
@@ -7,3 +7,5 @@ length = int(input("Enter the length of the table : "))
 height = int(input("Enter the height of the table : "))
 # Circumcision = str(2*(length + height))
 print ("Circumcision is " + str(2*(length + height)))
+
+
